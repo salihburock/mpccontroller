2133f703f426ec5600d2a5edf5e733bbd5a13059
salihburock <drgaabl@gmail.com>
2024-09-16 18:03:56 +0300

    Added toggle for mpd

--- app/src/main/java/com/mpc/mpccontroller/MainActivity.java
+++ app/src/main/java/com/mpc/mpccontroller/MainActivity.java
@@ -1,10 +1,8 @@
 package com.mpc.mpccontroller;
 
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.ResultReceiver;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
